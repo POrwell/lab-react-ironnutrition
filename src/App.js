74c366eb645b059b24335c117e4fa650fdfc2e1f
Phoebe Orwell <phoebe.orwell@gmail.com>
2022-11-16 14:59:22 +0100

    Iteration2 complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import foodsJSON from "./foods.json";
 import {v4 as uuidv4} from "uuid";
+import FoodBox from './components/Foodbox';
 
 function App() {
   const [foods, setFoods] = useState(foodsJSON)
@@ -13,6 +14,12 @@ function App() {
   <p>{food.name}</p>
   <img src={food.image} width={50} />
 </div> )}
+<FoodBox food={ {
+  name: "Orange",
+  calories: 85,
+  image: "https://i.imgur.com/abKGOcv.jpg",
+  servings: 1
+}} />
 </div>
   );
 }
