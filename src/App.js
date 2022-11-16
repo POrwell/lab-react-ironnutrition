d3ebb2de24c0ef616911bfa6945a39fb97adb34f
Phoebe Orwell <phoebe.orwell@gmail.com>
2022-11-16 14:46:38 +0100

    Iteration1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import foodsJSON from "./foods.json";
+import {v4 as uuidv4} from "uuid";
 
 function App() {
+  const [foods, setFoods] = useState(foodsJSON)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Food List</h1>
+    {foods.map((food) => 
+      <div key={uuidv4()}>
+  <p>{food.name}</p>
+  <img src={food.image} width={50} />
+</div> )}
+</div>
   );
 }
 
